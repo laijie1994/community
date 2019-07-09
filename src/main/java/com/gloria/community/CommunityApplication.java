587bfbc86aaedf39f4208632986e08b84b410bf4
laijie <laijie1994@163.com>
2019-07-09 17:00:43 +0800

    first commit

---
+++ src/main/java/com/gloria/community/CommunityApplication.java
@@ -0,0 +1,22 @@
+package com.gloria.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * @MethodName
+ * @Description //TODO
+ * @Param
+ * @return
+ * @Author jeremy.lai
+ * @Date 4:48 PM 2019/7/9
+ **/
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gloria/community/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.gloria.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author jeremy.lai
+ * @Date 2019/7/9 4:46 PM
+ * @Version 1.0
+ **/
+
+@Controller
+public class HelloController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
